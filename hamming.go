ad845849368c58066d0fe38f9ac6de491c7cc70e
Harish S <saharish@athenahealth.com>
2020-10-08 16:36:57 +0530

    hamming distance solved

---
+++ hamming.go
@@ -0,0 +1,19 @@
+package hamming
+
+import "errors"
+//Distance function
+func Distance(a, b string) (int, error) {
+	
+	if len(a) != len(b) {
+		return -1, errors.New("not equal")
+	}
+	
+	count := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	
+	return count, nil
+}
